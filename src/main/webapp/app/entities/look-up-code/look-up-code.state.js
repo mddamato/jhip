f57839cf4767a6fa8533ba43dc9d1360cf7a2a28
admin <mike@localhost.localdomain>
2016-06-17 15:20:32 -0400

    commit

--- src/main/webapp/app/entities/look-up-code/look-up-code.state.js
+++ src/main/webapp/app/entities/look-up-code/look-up-code.state.js
@@ -9,6 +9,12 @@
 
     function stateConfig($stateProvider) {
         $stateProvider
+
+
+
+
+
+
         .state('look-up-code', {
             parent: 'entity',
             url: '/look-up-code',
@@ -26,29 +32,11 @@
             resolve: {
             }
         })
-        .state('look-up-code-detail', {
-            parent: 'entity',
-            url: '/look-up-code/{id}',
-            data: {
-                authorities: ['ROLE_USER'],
-                pageTitle: 'LookUpCode'
-            },
-            views: {
-                'content@': {
-                    templateUrl: 'app/entities/look-up-code/look-up-code-detail.html',
-                    controller: 'LookUpCodeDetailController',
-                    controllerAs: 'vm'
-                }
-            },
-            resolve: {
-                entity: ['$stateParams', 'LookUpCode', function($stateParams, LookUpCode) {
-                    return LookUpCode.get({id : $stateParams.id}).$promise;
-                }]
-            }
-        })
-        .state('look-up-code.new', {
+
+
+        .state('look-up-code.list', {
             parent: 'look-up-code',
-            url: '/new',
+            url: '/list',
             data: {
                 authorities: ['ROLE_USER']
             },
@@ -66,59 +54,13 @@
                             };
                         }
                     }
-                }).result.then(function() {
+                }).result.then(
+
+                function() {
                     $state.go('look-up-code', null, { reload: true });
-                }, function() {
+                },
+                function() {
                     $state.go('look-up-code');
-                });
-            }]
-        })
-        .state('look-up-code.edit', {
-            parent: 'look-up-code',
-            url: '/{id}/edit',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/look-up-code/look-up-code-dialog.html',
-                    controller: 'LookUpCodeDialogController',
-                    controllerAs: 'vm',
-                    backdrop: 'static',
-                    size: 'lg',
-                    resolve: {
-                        entity: ['LookUpCode', function(LookUpCode) {
-                            return LookUpCode.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('look-up-code', null, { reload: true });
-                }, function() {
-                    $state.go('^');
-                });
-            }]
-        })
-        .state('look-up-code.delete', {
-            parent: 'look-up-code',
-            url: '/{id}/delete',
-            data: {
-                authorities: ['ROLE_USER']
-            },
-            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
-                $uibModal.open({
-                    templateUrl: 'app/entities/look-up-code/look-up-code-delete-dialog.html',
-                    controller: 'LookUpCodeDeleteController',
-                    controllerAs: 'vm',
-                    size: 'md',
-                    resolve: {
-                        entity: ['LookUpCode', function(LookUpCode) {
-                            return LookUpCode.get({id : $stateParams.id}).$promise;
-                        }]
-                    }
-                }).result.then(function() {
-                    $state.go('look-up-code', null, { reload: true });
-                }, function() {
-                    $state.go('^');
                 });
             }]
         });
